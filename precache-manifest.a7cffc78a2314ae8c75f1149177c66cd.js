51eb0ede28118a6472dd038fb45c81d6323b2df0
Meital Lazarovich <lazarovich.meital@gmail.com>
2019-12-19 18:11:04 +0200

    Updates

---
+++ precache-manifest.a7cffc78a2314ae8c75f1149177c66cd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "037d2273d68799180575abd5b55dafd4",
+    "url": "/mister-bitcoin/index.html"
+  },
+  {
+    "revision": "70b111897fcc53dd122b",
+    "url": "/mister-bitcoin/static/css/main.01c51e02.chunk.css"
+  },
+  {
+    "revision": "04291f900c5393098004",
+    "url": "/mister-bitcoin/static/js/2.2392061a.chunk.js"
+  },
+  {
+    "revision": "78ddf5a465591a03b9601ba2ddd63415",
+    "url": "/mister-bitcoin/static/js/2.2392061a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "70b111897fcc53dd122b",
+    "url": "/mister-bitcoin/static/js/main.dab907e0.chunk.js"
+  },
+  {
+    "revision": "f4318751123b69a2405c",
+    "url": "/mister-bitcoin/static/js/runtime-main.3a1417f1.js"
+  }
+]);
